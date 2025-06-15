db4740ed195f5c7740046bd0c316561338c4e416
gabrielolivra <gabernardinooliveira@gmail.com>
2025-06-15 08:35:49 -0300

    adjust prefix api

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.setGlobalPrefix('');
+  app.setGlobalPrefix('/api');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,6 +21,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
